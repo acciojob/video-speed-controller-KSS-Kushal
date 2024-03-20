4f2bd014526be5ebfa33f614a875330f7eaed732
Kushal Ghosh <115364340+KSS-Kushal@users.noreply.github.com>
2024-03-20 21:46:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,3 +7,34 @@ const inputs = document.querySelectorAll('.controls input');
 
     inputs.forEach(input => input.addEventListener('change', handleUpdate));
     inputs.forEach(input => input.addEventListener('mousemove', handleUpdate));
+
+video.addEventListener('timeupdate', updateProgressBar);
+function updateProgressBar() {
+    const progressPercent = (video.currentTime / video.duration) * 100;
+    progressBar.style.flexBasis = `${progressPercent}%`;
+}
+
+toggle.addEventListener('click', togglePlay);
+function togglePlay() {
+    const method = video.paused ? 'play' : 'pause';
+    video[method]();
+}
+
+volume.addEventListener('input', handleVolumeChange);
+function handleVolumeChange() {
+    video.volume = this.value;
+}
+
+playbackSpeed.addEventListener('input', handleSpeedChange);
+function handleSpeedChange() {
+    video.playbackRate = this.value;
+}
+
+rewind.addEventListener('click', handleRewind);
+function handleRewind() {
+    video.currentTime -= 10;
+}
+skip.addEventListener('click', handleSkip);
+function handleSkip() {
+    video.currentTime += 25;
+}
